5a82d31b0732b96c7a4fd29425f0a28f0cb383d0
Dmitry Zyryanov <cshlovjah@gmail.com>
2018-07-25 16:52:02 +0500

    Вечер

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use(express.static(path.join(__dirname, 'dist/')));
 
-app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/', express.static(path.join(__dirname, 'dist/')));
 app.use('/api/v1/articles', apiRouterArticles);
 
 // catch 404 and forward to error handler
